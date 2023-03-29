7d25738404d6e137910a6eb68776fa4ce07cd45c
Raymond Hernandez <67213974+rayherna18@users.noreply.github.com>
2023-03-28 23:54:01 -0400

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./index.css">
+    <title>About</title>
+</head>
+<body>
+    <main class="bg"></main>
+    <div class="loginBox">
+        <p id="about">Leader's Name:<br> Raymond Hernandez</p>
+        <p id="about">Project Name:<br> Wheel of Fortune Browser Edition</p>
+        <p id="about">Description:<br> A simple Wheel of Fortune game that uses forms and sessions to drive the game. Includes login and leaderboard</p>
+        <p id="about">David Roy:<br>Form Handling & Video Presentation</p>
+        <p id="about">Humayra Hussain:<br> Game Animations and CSS, Powerpoint and Video Presentation </p>
+        <p id="about">Raymond Hernandez:<br> Login, Grid Display and Powerpoint</p>
+        <p id="about">Zack Ansari:<br> Sessions & Leaderboard</p>
+    </div>
+    <?php
+    include './footer.php'
+    ?>
+</body>
+</html>
