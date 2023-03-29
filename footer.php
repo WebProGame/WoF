5204989022b7caf82481e7b6025364cd32365a6c
Raymond Hernandez <67213974+rayherna18@users.noreply.github.com>
2023-03-28 23:31:12 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <ul class="footer">
     <li><a href="index.php" id="regLink">Home</a></li>
     <li><a href="register.php" id="regLink">Registration</a></li>
+    <li><a href="aboutus.php" id="regLink">About Us</a></li>
     <li><a href="https://roulettedoc.com/wof-rules" target="_blank" id="regLink">How to Play</a></li>
-    <li><a href="register.php" id="regLink">Leaderboard</a></li>
-    <li><a href="register.php" id="regLink">Presentation</a></li>
+    <li><a href="leaderboard.php" id="regLink">Leaderboard</a></li>
+    <li><a href="https://docs.google.com/presentation/d/15lXhZGoE8bVb3uWObG29SqfGAyP1P4q17WJ6gx-A6wo/edit#slide=id.p" id="regLink">Presentation</a></li>
     <li><a href="logout.php" id="regLink">Logout</a></li>
 </ul>
